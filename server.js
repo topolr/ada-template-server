c86e4498103e7ef540a9f125033bd7ac1865a38a
hou80houzhu <hou80houzhu@vip.qq.com>
2017-12-06 11:08:07 +0800

    init

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ let bodyParser = require("body-parser");
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 
-app.use(express.static(require("path").resolve(__dirname, './app/dist')));
 app.use(cookieParser('admin'));
 app.use(session({
     secret: 'admin',
@@ -13,8 +12,5 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", (req, res) => {
-    res.send(require("fs").readFileSync(require("path").resolve(__dirname, "./app/dist/index.html"), "utf-8"));
-});
 
 module.exports = app;
